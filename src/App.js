f38a393e7b9d8de389c0baae7ed5d4a9b7f7ca13
Elisa <elisangli11@gmail.com>
2021-06-30 23:23:12 -0400

    Add react router

--- src/App.js
+++ src/App.js
@@ -1,14 +1,31 @@
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.css";
 
 import Header from "../src/components/Style/Header";
 import Footer from "../src/components/Style/Footer";
+import AboutUs from "../src/components/AboutUs/AboutUs";
+import Terms from "../src/components/Terms/Terms";
+import Registration from "../src/components/Registration/Registration";
 
 function App() {
   return (
-    <div>
-      <Header />
-      <Footer />
-    </div>
+    <Router>
+      <div>
+        <Header />
+        <Switch>
+          <Route path="/about">
+            <AboutUs />
+          </Route>
+          <Route path="/terms">
+            <Terms />
+          </Route>
+          <Route path="/registration">
+            <Registration />
+          </Route>
+        </Switch>
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
