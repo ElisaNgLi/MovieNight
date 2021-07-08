297d11d21a640930ecc13f1657a0e44b5c0e68ba
Elisa <elisangli11@gmail.com>
2021-07-08 14:19:27 -0400

    add so it doesnt repeat at random

--- src/components/Home/FeatureMovie.js
+++ src/components/Home/FeatureMovie.js
@@ -1,35 +1,71 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import ScrollMenu from "react-horizontal-scrolling-menu";
 
-const FeatureMovie = ({ movies }) => {
-  const random = (movies) => {
-    let results = [];
-    for (let i = 0; i < 6; i++) {
-      let rand = movies[Math.floor(Math.random() * 16)];
-      results.push(rand);
+import SubTitle from "../Style/SubTitle";
+
+const FeatureMovie = () => {
+  const [movies, setMovies] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    fetch("http://localhost:8000/movies")
+      .then((res) => {
+        return res.json();
+      })
+      .then((data) => {
+        setMovies(data);
+        setIsLoading(false);
+      })
+      .catch((err) => {
+        setIsLoading(false);
+        console.log(err.message);
+      });
+  }, []);
+
+  const random = (max, movies) => {
+    let result = [];
+    let counter = 0;
+
+    while (counter < max) {
+      let rand = movies[Math.floor(Math.random() * movies.length)];
+      if (!result.some((an) => an === rand)) {
+        result.push(rand);
+        counter++;
+      }
     }
-    return results;
+    return result;
   };
-  return (
-    <div className="container">
-      <ScrollMenu
-        arrowLeft={<div style={{ fontSize: "30px" }}>{" < "}</div>}
-        arrowRight={<div style={{ fontSize: "30px" }}>{" > "}</div>}
-        data={random(
-          movies.map((data, index) => (
-            <img
-              className="mx-2"
-              key={index}
-              src={data.poster_path}
-              alt={data.title}
-              width="200"
-              height="200"
-            />
-          ))
-        )}
-      />
-    </div>
-  );
+
+  if (isLoading) {
+    return (
+      <div className="container">
+        <p>isLoading...</p>
+      </div>
+    );
+  } else {
+    return (
+      <div className="container my-3">
+        <SubTitle name="Feature Movies" />
+        <ScrollMenu
+          arrowLeft={<div style={{ fontSize: "30px" }}>{" < "}</div>}
+          arrowRight={<div style={{ fontSize: "30px" }}>{" > "}</div>}
+          data={random(
+            6,
+            movies.map((data, index) => (
+              <img
+                className="mx-2"
+                key={index.toString()}
+                src={data.poster_path}
+                alt={data.title}
+                width="200"
+                height="200"
+              />
+            ))
+          )}
+        />
+      </div>
+    );
+  }
 };
 
 export default FeatureMovie;
--- src/components/Home/FeatureTv.js
+++ src/components/Home/FeatureTv.js
@@ -1,37 +1,71 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import ScrollMenu from "react-horizontal-scrolling-menu";
 
-const FeatureTv = ({ tv }) => {
-  const random = (tv) => {
-    let results = [];
+import SubTitle from "../Style/SubTitle";
 
-    for (let i = 0; i < 6; i++) {
-      let rand = tv[Math.floor(Math.random() * tv.length + 1)];
-      results.push(rand);
+const FeatureTv = () => {
+  const [tv, setTv] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    fetch("http://localhost:8000/tv")
+      .then((res) => {
+        return res.json();
+      })
+      .then((data) => {
+        setTv(data);
+        setIsLoading(false);
+      })
+      .catch((err) => {
+        setIsLoading(false);
+        console.log(err.message);
+      });
+  }, []);
+
+  const random = (max, tv) => {
+    let result = [];
+    let counter = 0;
+
+    while (counter < max) {
+      let rand = tv[Math.floor(Math.random() * tv.length)];
+      if (!result.some((an) => an === rand)) {
+        result.push(rand);
+        counter++;
+      }
     }
-    return results;
+    return result;
   };
 
-  return (
-    <div className="container">
-      <ScrollMenu
-        arrowLeft={<div style={{ fontSize: "30px" }}>{" < "}</div>}
-        arrowRight={<div style={{ fontSize: "30px" }}>{" > "}</div>}
-        data={random(
-          tv.map((data, index) => (
-            <img
-              className="mx-2"
-              key={index}
-              src={data.poster_path}
-              alt={data.title}
-              width="200"
-              height="200"
-            />
-          ))
-        )}
-      />
-    </div>
-  );
+  if (isLoading) {
+    return (
+      <div className="container">
+        <p> is loading</p>
+      </div>
+    );
+  } else {
+    return (
+      <div className="container my-3">
+        <SubTitle name="Feature Tv Shows" />
+        <ScrollMenu
+          arrowLeft={<div style={{ fontSize: "30px" }}>{" < "}</div>}
+          arrowRight={<div style={{ fontSize: "30px" }}>{" > "}</div>}
+          data={random(
+            6,
+            tv.map((data, index) => (
+              <img
+                className="mx-2"
+                key={index.toString()}
+                src={data.poster_path}
+                alt={data.name}
+                width="200"
+                height="200"
+              />
+            ))
+          )}
+        />
+      </div>
+    );
+  }
 };
 
 export default FeatureTv;
