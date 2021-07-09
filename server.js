69fc489a49ce8be010ba39ad76ce07cc1e15d268
Elisa <elisangli11@gmail.com>
2021-07-08 21:54:22 -0400

    back to original

--- server.js
+++ server.js
@@ -3,9 +3,17 @@ const server = jsonServer.create();
 const middlewares = jsonServer.defaults();
 const path = require("path");
 const router = jsonServer.router(path.join(__dirname, "/data/db.json"));
+const express = require("express");
+const PORT = process.env.PORT || 8000;
 
 server.use(middlewares);
 server.use(router);
-server.listen(8000, () => {
+
+server.use(express.static(__dirname + "public"));
+server.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
+});
+
+server.listen(PORT, () => {
   console.log("JSON Server is running");
 });
